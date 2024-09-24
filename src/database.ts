f9e7c372fee6a6602bb7529a22adbcfc357e1453
iagopg <iago.pereirasp17@gmail.com>
2024-09-23 22:54:04 -0300

    feat: added zod, dotenv and some updates,

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,22 @@
-import {knex as setupKnex} from 'knex'
+import 'dotenv/config'
+import { knex as setupKnex, type Knex } from 'knex'
+import { env } from './env'
 
-export const knex = setupKnex({
-    client:'sqlite',
-    connection:{
-        filename: './tmp/app.db'
-    }
-})
+
+
+export const config: { [key: string]: Knex.Config } = {
+  development: {
+    client: 'sqlite3', 
+    connection: {
+      filename: env.DATABASE_URL
+    },
+    migrations: {
+      directory: './db/migrations',
+      extension:'ts'
+    },
+    useNullAsDefault: true, 
+  },
+}
+
+
+export const knex = setupKnex(config.development)
