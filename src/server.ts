0c70d649a9b5146b390bd6d2459fb12ef7aab825
iagopg <iago.pereirasp17@gmail.com>
2024-09-30 18:22:53 -0300

    fix: port fixed

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import { app } from "./app"
-import { env } from "./env"
 
 
-app.listen({port: env.PORT,host: ("RENDER" in process.env) ? '0.0.0.0' : 'localhost'}).then(() => {
+app.listen({port: Number(process.env.PORT) || 3000, host: ("RENDER" in process.env) ? '0.0.0.0' : 'localhost'}).then(() => {
 	console.log('server running')
 })
